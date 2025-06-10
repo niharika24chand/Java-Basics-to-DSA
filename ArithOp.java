b0b66943907ec98febb8c498e64b52196790d2b0
niharika24chand <nhrkchnd@gmail.com>
2025-06-10 23:45:24 +0530

    Added

---
+++ ArithOp.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class ArithOp  //ArithOp = Arithmetic Operations 
+{
+   public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int num1 = 0, num2 = 0, sum = 0;
+        System.out.println("Enter two numbers: ");
+        num1 = sc.nextInt();
+        num2 = sc.nextInt();
+        System.out.println("Sum = " + (num1+num2)); 
+        System.out.println("Subtraction = " + (num1-num2)); 
+        System.out.println("Product = " + (num1*num2)); 
+        System.out.println("Division = " + (num1/num2)); 
+        System.out.println("Remainder = " + (num1%num2)); 
+    }
+}
